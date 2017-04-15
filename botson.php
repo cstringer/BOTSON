7fe99f0445c9fb789b94559cc8d5abe4fd6cc9ef
Chris Stringer <cstringer@caltesting.org>
2017-04-15 11:14:56 -0500

    Generously enhances configurability in true KB fashion: multiple sentences, paragraphs, command-line options.. the works

--- botson.php
+++ botson.php
@@ -1,19 +1,9 @@
-#!/usr/bin/php -f
+#!/usr/bin/env php
 <?php
-error_reporting(E_ALL);
-srand();
-
-// sentence types
-$sentenceTypes = array('SV', 'SVO', 'SVAO', 'SVP');
-
-// use a random sentence type
-$senType = getRandom($sentenceTypes);
-
-// should it be a compound sentence?
-$compound = (rand() % 2 == 0) ? TRUE : FALSE;
-
-// if so, use a conjunction?
-$useConj = (rand() % 4 == 0) ? FALSE : TRUE;
+define('NUM_SENTENCES',  1);
+define('NUM_PARAGRAPHS', 1);
+define('FREQ_COMPOUND',  2);
+define('FREQ_CONJ',      4);
 
 // load word lists from external files
 $verbs = file('wl-verbs.txt');
@@ -22,15 +12,106 @@ $jargon = file('wl-ux-terms.txt');
 $cal = file('wl-cal.txt');
 $conj = file('wl-conj.txt');
 
+// sentence types
+$sentenceTypes = array('SV', 'SVO', 'SVAO', 'SVP');
+
 // end-of-sentence punctuation
 $eosPunctuation = array('.','!','?','.');
 
-// main
-$sentence = array();
-buildSentence($sentence, $senType, $compound, $useConj);
-printSentence($sentence);
+// set script error reporting level
+error_reporting(E_ALL);
 
-// build a sentence of the given type
+// seed random number generator
+srand();
+
+/** Main */
+main();
+function main() {
+    global $argc, $argv, $sentenceTypes;
+
+    // number of sentences
+    $numSentences = NUM_SENTENCES;
+
+    // number of paragraphs
+    $numParagraphs = NUM_PARAGRAPHS;
+
+    // should it be a compound sentence?
+    $compound = randomBool(FREQ_COMPOUND);
+
+    // if so, use a conjunction?
+    $useConj = randomBool(FREQ_CONJ, TRUE);
+
+    // randomize all options
+    $randomizeAll = FALSE;
+
+    // default to random sentence type(s)
+    $type = NULL;
+
+    // process command-line args
+    for ($a = 1; $a < $argc; $a++) {
+        switch($argv[$a]) {
+            case '-n':
+            case '--num-sentences':
+                $numSentences = $argv[++$a];
+                break;
+
+            case '-p':
+            case '--num-paragraphs':
+                $numParagraphs = $argv[++$a];
+                break;
+
+            case '-c':
+            case '--compound':
+                $compound = TRUE;
+                break;
+
+            case '-j':
+            case '--use-conjunction':
+                $useConj = TRUE;
+                break;
+
+            case '-r':
+            case '--randomize':
+                $randomizeAll = TRUE;
+                break;
+
+            case '-t':
+            case '--type':
+                $t = $argv[++$a];
+                if (in_array($t, $sentenceTypes)) {
+                    $type = $t;
+                    $numSentences = 1;
+                    $numParagraphs = 1;
+                }
+                break;
+        }
+    }
+
+    for (; $numParagraphs > 0; $numParagraphs--) {
+        for ($n = $numSentences; $n > 0; $n--) {
+            $sentence = array();
+            if ($randomizeAll) {
+                $compound = randomBool(FREQ_COMPOUND);
+                $useConj = randomBool(FREQ_CONJ, TRUE);
+                $type = NULL;
+            }
+            buildSentence($sentence, $type, $compound, $useConj);
+            printSentence($sentence);
+            if ($numSentences > 1) {
+                $randomizeAll = TRUE;
+                echo " ";
+            }
+        }
+        if ($numParagraphs > 1) {
+            echo "\n";
+        }
+        echo "\n";
+    }
+
+    exit();
+}
+
+/** Build a sentence of the given type */
 function buildSentence(&$sentence, $type = NULL, $compound = FALSE, $useConj = TRUE) {
     global $sentenceTypes, $cal, $verbs, $jargon, $preps, $conj;
     if (!$type) {
@@ -62,11 +143,11 @@ function buildSentence(&$sentence, $type = NULL, $compound = FALSE, $useConj = T
             $sentence[] = getRandom($cal);
             $sentence[] = getRandom($verbs);
             break;
-        }
+    }
 
-    /* For compound sentences, either add a comma after the last word
-     *  and add a conjunction, or only add a semicolon after last word
-     *  and recurse to add another sentence */
+    /* For compound sentences: either add a comma after the last word
+     *  and add a conjunction, or only add a semicolon after last word;
+     *  recurse to add another sentence */
     if ($compound) {
         $lastWord = array_pop($sentence);
         if ($useConj) {
@@ -79,19 +160,27 @@ function buildSentence(&$sentence, $type = NULL, $compound = FALSE, $useConj = T
     }
 }
 
-/* Print the sentence words (first capitalized) separated by spaces
- *  with the given (or random) end-of-sentence punctuation */
+/**
+ * Print the sentence words (first capitalized) separated by spaces
+ *  with the given (or random) end-of-sentence punctuation
+ */
 function printSentence($sentence, $es_punc = NULL) {
     global $eosPunctuation;
     if (!$es_punc) {
         $es_punc = getRandom($eosPunctuation);
     }
-    echo ucfirst(implode(' ', $sentence)) . $es_punc . "\n";
+    echo ucfirst(implode(' ', $sentence)) . $es_punc;
 }
 
-// get a random entry from the given array (with any EOL whitespace trimmed)
+/** Get a random entry from the given array (with any EOL whitespace trimmed) */
 function getRandom($array = array()) {
     return rtrim($array[array_rand($array)]);
+}
+
+/** Produce a random boolean, weighted by frequency with optional exclusivity */
+function randomBool($freq = 2, $exclusive = FALSE) {
+    $rb = (rand() % $freq == 0) ? TRUE : FALSE;
+    return $exclusive ? !$rb : $rb;
 }
 
 ?>
