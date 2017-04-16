a2ecde1cfe97c530de080f86055e729a82900422
Botson <botsonlives@p10m.com>
2017-04-16 14:39:00 -0400

    Rectifies API documentation metrics

--- index.php
+++ index.php
@@ -1,25 +1,24 @@
 <?php
-
+// number of sentences
 $numSent = 1;
 if (isset($_GET['s']) && preg_match('/^[0-9]+$/', $_GET['s'])) {
     $numSent = $_GET['s'];
 }
 
+// number of paragraphs
 $numPara = 1;
 if (isset($_GET['p']) && preg_match('/^[0-9]+$/', $_GET['p'])) {
     $numPara = $_GET['p'];
 }
 
+// call command-line script, capture output, remove trailing newling
 $output = rtrim(`./botson.php -n $numSent -p $numPara`);
-$output = str_replace("\n","<br>",$output);
 
+// convert remiaing newlines to linebreaks
+$output = nl2br($output);
+
+// output JSON-encoded object
 header('Content-type: text/json; charset=utf-8');
-echo json_encode(
-	array(
-        'text' => $output
-	)
-);
-
-exit();
+echo json_encode(array('text' => $output));
 
 ?>
